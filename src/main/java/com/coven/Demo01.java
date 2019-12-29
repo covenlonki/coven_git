30023800b1d215f37fa6154b7860018ad33ae1b7
itcast <bos@itcast.cn>
2019-12-29 18:12:18 +0800

    Merge remote-tracking branch 'remotes/origin/master' into 12-29-test01
    # Conflicts:
    #	src/main/java/com/coven/Demo01.java

--- src/main/java/com/coven/Demo01.java
+++ src/main/java/com/coven/Demo01.java
@@ -1,8 +1,7 @@
 package com.coven;
 
 public class Demo01 {
-    //master 02
-    //12-29-test01
+    //12-29-test2222
      public static void main(String[] args) {
          System.out.println("你好!");
      }
